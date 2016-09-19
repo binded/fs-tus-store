c6ae833c968bd365903f177fc9989cd1b482805f
Olivier Lalonde <olalonde@gmail.com>
2016-09-18 20:55:01 -0700

    use abstract-tus-store for tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,72 +1,13 @@
-import test from 'blue-tape'
+import testStore from 'abstract-tus-store'
 import rimraf from 'rimraf'
 import mkdirp from 'mkdirp'
-import str from 'string-to-stream'
-
 import initFsStore from '../src'
 
-const resetStore = () => {
+const setup = () => {
   const directory = `${__dirname}/.data`
   rimraf.sync(directory)
   mkdirp.sync(directory)
   return initFsStore({ directory })
 }
 
-const store = resetStore()
-
-test('info - unknown key', (t) => {
-  store
-    .info('unknown-key')
-    .catch((err) => {
-      t.ok(err instanceof Error)
-      t.end()
-    })
-})
-
-test('info - foo- unknown key', (t) => {
-  store
-    .info('foo')
-    .catch((err) => {
-      t.ok(err instanceof Error)
-      t.end()
-    })
-})
-
-test('create foo', () => (
-  store
-    .create('foo', { uploadLength: 'bar'.length })
-))
-
-test('info foo', (t) => (
-  store
-    .info('foo')
-    .then(({ uploadOffset, uploadLength }) => {
-      t.equal(uploadOffset, 0)
-      t.equal(uploadLength, 3)
-    })
-))
-
-test('write ba to foo', () => (
-  store
-    .write('foo', str('ba'))
-))
-
-test('info foo', (t) => (
-  store
-    .info('foo')
-    .then(({ uploadOffset, uploadLength }) => {
-      t.equal(uploadOffset, 2)
-      t.equal(uploadLength, 3)
-    })
-))
-
-test('write r to foo', () => store.write('foo', str('r')))
-
-test('info foo', (t) => (
-  store
-    .info('foo')
-    .then(({ uploadOffset, uploadLength }) => {
-      t.equal(uploadOffset, 3)
-      t.equal(uploadLength, 3)
-    })
-))
+testStore({ setup })
