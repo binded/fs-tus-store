4eda88547767b3ae0cd276c8c97f9b8b8c9bfac1
Olivier Lalonde <olalonde@gmail.com>
2016-09-21 14:51:58 -0700

    feat(*): upgrade to v2 api
    BREAKING CHANGES!

--- src/index.js
+++ src/index.js
@@ -1,80 +1,170 @@
-import path from 'path'
 import fs from 'fs'
-import pump from 'pump'
+import { PassThrough } from 'stream'
+import { errors } from 'abstract-tus-store'
+import uuid from 'uuid'
+import str from 'string-to-stream'
+import fsBlobStore from 'fs-blob-store'
+import initDebug from 'debug'
+import concat from 'concat-stream'
+import MeterStream from 'meterstream'
+import { SizeStream } from 'common-streams'
+import { join, resolve as resolvePath } from 'path'
 
-// promisify
-const readFile = (file) => new Promise((resolve, reject) => {
-  fs.readFile(file, (err, data) => {
-    if (err) return reject(err)
-    resolve(data)
-  })
-})
-const writeFile = (file, data) => new Promise((resolve, reject) => {
-  fs.writeFile(file, data, (err) => {
-    if (err) return reject(err)
-    resolve(data)
-  })
-})
-const stat = (file) => new Promise((resolve, reject) => {
-  fs.stat(file, (err, statObj) => {
-    if (err) return reject(err)
-    resolve(statObj)
-  })
-})
+const debug = initDebug('fs-tus-store')
 
+// TODO: prevent concurrent operations...
 export default ({
   directory = '.fs-tus-store',
 } = {}) => {
-  const absoluteDir = path.resolve(directory)
-  // Returns { uploadOffset[, uploadLength] }
+  const absoluteDir = resolvePath(directory)
+  const blobStore = fsBlobStore(absoluteDir)
 
-  // TODO: convert key to base64 or validate it?
-  const keyPath = (key) => path.join(absoluteDir, key)
-  const keyInfoPath = (key) => `${keyPath(key)}.info`
+  const keyToPath = (key) => join(absoluteDir, key)
+  const uploadIdToKey = (uploadId) => `.uploads/${uploadId}.info`
+  const uploadIdToDataKey = (uploadId) => `.uploads/${uploadId}`
 
-  const getKeyInfo = (key) => readFile(keyInfoPath(key))
-    .then(data => JSON.parse(data))
-
-  const setKeyInfo = (key, infoObj) => Promise.resolve()
-    .then(() => JSON.stringify(infoObj))
-    .then(data => writeFile(keyInfoPath(key), data))
-
-  const getKeyOffset = (key) => stat(keyPath(key))
-    .then(({ size }) => size)
-
-  const touchKey = (key) => writeFile(keyPath(key), '')
-
-  // TODO: fail if key already exists?
-  const create = (key, { uploadLength, uploadMetadata } = {}) => (
-    setKeyInfo(key, { uploadLength, uploadMetadata })
-      .then(() => touchKey(key))
-  )
-
-  const info = key => getKeyInfo(key)
-    .then(infoObj => (
-      getKeyOffset(key)
-        .then((uploadOffset) => ({
-          ...infoObj,
-          uploadOffset,
-        }))
-    ))
-
-  const write = (key, rs) => new Promise((resolve, reject) => {
-    const ws = fs.createWriteStream(keyPath(key), {
-      flags: 'a', // append to file
-    })
-    pump(rs, ws, (err) => {
+  const put = (key, data) => new Promise((resolve, reject) => {
+    const ws = blobStore.createWriteStream(key, (err) => {
       if (err) return reject(err)
       resolve()
     })
+    str(data).pipe(ws)
   })
 
-  const createReadStream = (key) => fs.createReadStream(keyPath(key))
+  const saveUpload = async (uploadId, upload) => {
+    const json = JSON.stringify(upload)
+    await put(uploadIdToDataKey(uploadId), '')
+    await put(uploadIdToKey(uploadId), json)
+  }
+
+  const getUpload = async (uploadId) => {
+    const key = uploadIdToKey(uploadId)
+    const keyExists = await new Promise((resolve, reject) => {
+      blobStore.exists(key, (err, result) => {
+        if (err) return reject(err)
+        resolve(result)
+      })
+    })
+    if (!keyExists) throw new errors.UploadNotFound(uploadId)
+    const json = await new Promise((resolve, reject) => {
+      const rs = blobStore
+        .createReadStream(key)
+        .on('error', reject)
+      rs.pipe(concat((result) => {
+        resolve(result)
+      }))
+    })
+    return JSON.parse(json)
+  }
+
+  // TODO: fail if key already exists?
+  const create = async (key, {
+    uploadLength,
+    metadata = {},
+  } = {}) => {
+    const uploadId = uuid.v4()
+    const upload = {
+      key,
+      uploadLength,
+      metadata,
+    }
+    await saveUpload(uploadId, upload)
+    return { uploadId }
+  }
+
+  const getUploadOffset = async (uploadId) => {
+    const key = uploadIdToDataKey(uploadId)
+    return new Promise((resolve, reject) => {
+      fs.stat(keyToPath(key), (err, stats) => {
+        if (err) return reject(err)
+        resolve(stats.size)
+      })
+    })
+  }
+
+  const info = async uploadId => {
+    const upload = await getUpload(uploadId)
+    debug(upload)
+    const offset = await getUploadOffset(uploadId)
+      .catch(err => {
+        // we got the upload file but upload data does not exist
+        // that means the upload is actually completed.
+        if (err.code === 'ENOENT') {
+          return upload.uploadLength
+        }
+        throw err
+      })
+    return {
+      offset,
+      ...upload,
+    }
+  }
+
+  const createLimitStream = (uploadLength, offset) => {
+    if (typeof uploadLength === 'undefined') return new PassThrough()
+    const meterStream = new MeterStream(uploadLength - offset)
+    return meterStream
+  }
+
+  const completeUpload = async (uploadId, upload) => {
+    const oldPath = keyToPath(uploadIdToDataKey(uploadId))
+    const newPath = keyToPath(upload.key)
+    return new Promise((resolve, reject) => {
+      fs.rename(oldPath, newPath, (err) => {
+        if (err) return reject(err)
+        resolve()
+      })
+    })
+  }
+
+  const append = async (uploadId, rs, arg3, arg4) => {
+    // guess arg by type
+    const { expectedOffset, opts = {} } = (() => {
+      if (typeof arg3 === 'object') {
+        return { opts: arg3 }
+      }
+      return { expectedOffset: arg3, opts: arg4 }
+    })()
+    // need to do this asap to make sure we don't miss reads
+    const through = rs.pipe(new PassThrough())
+
+    debug('append opts', opts)
+
+    const upload = await getUpload(uploadId)
+    const offset = await getUploadOffset(uploadId)
+    if (Number.isInteger(expectedOffset)) {
+      // check if offset is right
+      if (offset !== expectedOffset) {
+        throw new errors.OffsetMismatch(offset, expectedOffset)
+      }
+    }
+    const limitStream = createLimitStream(upload.uploadLength, offset)
+    const bytesWritten = await new Promise((resolve, reject) => {
+      let size
+      const sizeStream = new SizeStream((s) => { size = s })
+      const ws = blobStore.createWriteStream({
+        key: uploadIdToDataKey(uploadId),
+        flags: 'a',
+      }, (err) => {
+        if (err) return reject(err)
+        resolve(size)
+      })
+      through.pipe(limitStream).pipe(sizeStream).pipe(ws)
+    })
+    const newOffset = offset + bytesWritten
+    if (newOffset === upload.uploadLength) {
+      await completeUpload(uploadId, upload)
+      return { offset: newOffset, complete: true }
+    }
+    return { offset: newOffset }
+  }
+
+  const createReadStream = (key) => blobStore.createReadStream(key)
 
   return {
     info,
     create,
-    write,
+    append,
     createReadStream,
   }
 }
